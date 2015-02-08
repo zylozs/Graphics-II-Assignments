c872c2396bc64b31644c6ecfef8d84abd9d6af05
Vincent <vincent.loignon@gmail.com>
2015-02-08 14:59:07 -0500

    - Added my comment to top of BaseObject3D

--- Assignment2/3DClasses/BaseObject3D.h
+++ Assignment2/3DClasses/BaseObject3D.h
@@ -3,6 +3,9 @@
 //
 // Writen by Adi Bar-Lev, 2013
 // EGP 300-101, Graphics Programming II  - skeleton project
+// 
+// Modified by Vincent Loignon, 2015
+// Added more modularity to it
 //
 // Base class that can handle 3D rendergin via Vertex and Index buffer
 //=============================================================================
@@ -38,10 +41,12 @@ protected:
 
 
 protected:
-    // Replace the code in the following methods
+    // Can be over-written, but isn't necessary for sub classes
     virtual void buildVertexBuffer( IDirect3DDevice9* gd3dDevice );
     virtual void buildIndexBuffer( IDirect3DDevice9* gd3dDevice );
 
+	// You should override these functions for each sub class!
+	// Add your vertices/indices to the vector passed to the function and it will draw the object for you
 	virtual void calculateVertexBuffer(std::vector<VertexPos>& vertices);
 	virtual void calculateIndexBuffer(std::vector<WORD>& indices);
 
@@ -55,7 +60,7 @@ public:
 	virtual void Update(float dt) {}
 
 	const D3DXVECTOR3& getCenterPos() { return m_CenterPos; }
-	void setCenterPos(FLOAT x, FLOAT y, FLOAT z);
+	void setCenterPos(FLOAT x, FLOAT y, FLOAT z); // Used to move a BaseObject3D to a position in World space by translating its World Matrix
 };
 //=============================================================================
 #endif // _BASE_OBJECT_3D_H
